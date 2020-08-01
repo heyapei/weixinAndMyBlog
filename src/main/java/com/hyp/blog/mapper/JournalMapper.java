ce779fe7f2a87979002242425898883d6f94d5d7
heyapei <15518901416@163.com>
2020-08-01 12:15:49 +0800

    创建了日志实体类

---
+++ src/main/java/com/hyp/blog/mapper/JournalMapper.java
@@ -0,0 +1,9 @@
+package com.hyp.blog.mapper;
+
+import com.hyp.blog.pojo.modal.Journal;
+import com.hyp.blog.utils.MyMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JournalMapper extends MyMapper<Journal> {
+}
